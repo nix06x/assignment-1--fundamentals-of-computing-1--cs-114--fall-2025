21b400c1032a0ebb9fbeff49d59e10c1f497241f
Nix Barreau <j.bizarre16@gmail.com>
2025-09-09 17:15:06 -0400

    Update Assignment1Program1.java
    Step 1

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,18 @@
 public class Assignment1Program1 {
+
   public static void main(String[] args) {
+
+    System.out.println("NNNNNNN        NNNNN     BBBBBBBBBBBBBBBB");
+    System.out.println("NNNNNNNN       NNNNN     BBBBBBBBBBBBBBBBB");
+    System.out.println("NNNNNNNNN      NNNNN     BBBBB        BBBBBB");
+    System.out.println("NNNNNNNNNN     NNNNN     BBBBB         BBBBBB");
+    System.out.println("NNNNNN NNNNN   NNNNN     BBBBB      BBBBBBBB");
+    System.out.println("NNNNNN  NNNNN  NNNNN     BBBBBBBBBBBBBB");
+    System.out.println("NNNNNN   NNNNNNNNNNN     BBBBB      BBBBBBBB");
+    System.out.println("NNNNNN    NNNNNNNNNN     BBBBB         BBBBBB");
+    System.out.println("NNNNNN     NNNNNNNNN     BBBBB        BBBBBB");
+    System.out.println("NNNNNN      NNNNNNNN     BBBBBBBBBBBBBBBBB");
+    System.out.println("NNNNNN       NNNNNNN     BBBBBBBBBBBBBBBB");
 
   }
 }
