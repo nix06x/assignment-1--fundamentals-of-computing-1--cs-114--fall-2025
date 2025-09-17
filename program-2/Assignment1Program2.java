dc90343e96f965d76097a0ea08fc0c586e9cc7a2
Nix Barreau <j.bizarre16@gmail.com>
2025-09-17 15:29:31 -0400

    Update Assignment1Program2.java
    Attempt 2

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -13,50 +13,12 @@ public class Assignment1Program2 {
 
     userInput = scan.nextLine();
 
-    final int STARTING_NUM = 9999;
+    scan.close();
+
     final int BASE_NUM = (Integer.parseInt(userInput));
-    int quotient = 0;
-    int remainder = 0;
-    int maxNumber = 0;
+    final int BASE_MAX = (BASE_NUM - 1);
 
-    int digitOne = 0;
-    int digitTen = 0;
-    int digitHundred = 0;
-    int digitThousand = 0;
-
-    remainder = (STARTING_NUM % BASE_NUM);
-    quotient = (STARTING_NUM / BASE_NUM);
-
-    digitOne = remainder;
-
-    if (remainder != 0){
-
-      remainder = (quotient % BASE_NUM);
-      quotient = (quotient / BASE_NUM);
-
-      digitTen = remainder;
-
-      if (remainder != 0){
-
-        remainder = (quotient % BASE_NUM);
-        quotient = (quotient % BASE_NUM);
-
-        digitHundred = remainder;
-
-        if (remainder != 0){
-
-          remainder = (quotient % BASE_NUM);
-          quotient = (quotient % BASE_NUM);
-
-          digitThousand = remainder;
-
-        }
-
-      }
-
-    }
-
-    maxNumber = ((digitThousand * 1000) + (digitHundred * 100) + (digitTen * 10) + (digitOne * 1));
+    int maxNumber = ((BASE_MAX * (BASE_NUM ^ 3)) + (BASE_MAX * (BASE_NUM ^ 2)) + (BASE_MAX * (BASE_NUM ^ 1)) + (BASE_MAX * (BASE_NUM ^ 0)));
 
     System.out.println("The maximum, 4-digit, base 10 number in base " + Integer.toString(BASE_NUM) + " is " + Integer.toString(maxNumber) + ".");
 
